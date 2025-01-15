06b1d089426f629764750d49457f20d1c806fea1
fluke <fluke@odds.team>
2025-01-15 19:23:40 +0700

    Initial commit

---
+++ POCUIKitApp/POCUIKitApp.h
@@ -0,0 +1,18 @@
+//
+//  POCUIKitApp.h
+//  POCUIKitApp
+//
+//  Created by flukeInwza on 15/1/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for POCUIKitApp.
+FOUNDATION_EXPORT double POCUIKitAppVersionNumber;
+
+//! Project version string for POCUIKitApp.
+FOUNDATION_EXPORT const unsigned char POCUIKitAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POCUIKitApp/PublicHeader.h>
+
+
